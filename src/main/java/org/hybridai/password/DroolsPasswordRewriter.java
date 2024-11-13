98490efb1bc805738f10e0f06d0fb28d5795ae62
mariofusco <mario.fusco@gmail.com>
2024-11-13 17:34:04 +0100

    fix temperature for password generator example

--- src/main/java/org/hybridai/password/DroolsPasswordRewriter.java
+++ src/main/java/org/hybridai/password/DroolsPasswordRewriter.java
@@ -6,7 +6,7 @@ import jakarta.inject.Singleton;
 public class DroolsPasswordRewriter {
 
     public String rewritePassword(String s) {
-        if (s.matches(".*[\\W_].*")) {
+        if (!s.matches(".*[\\W_].*")) {
             if (s.contains("e")) {
                 s = s.replace('e', '&');
             } else if (s.contains("a")) {
@@ -18,12 +18,28 @@ public class DroolsPasswordRewriter {
             }
         }
 
+        if (!s.matches(".*\\d.*")) {
+            if (s.contains("e")) {
+                s = s.replace('e', '3');
+            } else if (s.contains("o")) {
+                s = s.replace('o', '0');
+            } else if (s.contains("i")) {
+                s = s.replace('i', '1');
+            } else {
+                s = new java.util.Random().nextInt(0,9) + s;
+            }
+        }
+
         if (s.contains(" ")) {
             s = s.replace(' ', '_');
         }
 
         if (s.length() > 10) {
             s = s.substring(0, 8);
+        }
+
+        if (s.length() < 8) {
+            s = s + new java.util.Random().nextInt(0,9);
         }
 
         return s;
